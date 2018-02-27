3b743d15508ec9974f27ac1367ae79eeb52cc6a9
Govindan <arun.govindan@lnttechservices.com>
2018-02-27 13:36:14 -0500

    Test

--- ci/build_win32.bat
+++ ci/build_win32.bat
@@ -53,8 +53,12 @@ cd %ORIG_DIR%
 
 @rem deploy artifacts
 @rem based on: https://www.appveyor.com/docs/build-worker-api/#push-artifact
-if "%APPVEYOR%"=="True" (
-    if "%DEPLOY_ARTIFACTS%"=="True" (
+
+set cronUpload=Fasle 
+
+if "%APPVEYOR_SCHEDULED_BUILD%"=="True" if  "%APPVEYOR_REPO_NAME%"=="arun-govindan/pxcore" if "%APPVEYOR_REPO_BRANCH%"=="master"  set cronUpload=True
+  
+if %cronUpload%==True (
         @rem NSIS based installer
         appveyor PushArtifact "build-win32\\_CPack_Packages\\win32\\NSIS\\pxscene-setup.exe" -DeploymentName "installer" -Type "Auto" -Verbosity "Normal"
 
