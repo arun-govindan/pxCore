787808d97fad2aa9c924d43a6fad26f2b4c182ab
arun-govindan <arun.govindan@lnttechservices.com>
2018-03-02 13:56:06 -0500

    testing permission tests

--- ci/execute_linux.sh
+++ ci/execute_linux.sh
@@ -63,7 +63,7 @@ retVal=$?
 
 # Monitor testRunner ...
 count=0
-max_seconds=1000
+max_seconds=900
 
 while [ "$retVal" -ne 0 ] &&  [ "$count" -ne "$max_seconds" ]; do
 	printf "\n [execute_osx.sh] snoozing for 30 seconds (%d of %d) \n" $count $max_seconds
