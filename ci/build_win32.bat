b622787b5e14262c4f26f2cfa74b9913295ad6ba
Govindan <arun.govindan@lnttechservices.com>
2018-03-28 11:26:28 -0400

    test

--- ci/build_win32.bat
+++ ci/build_win32.bat
@@ -41,16 +41,16 @@ cd build-win32
 if "%APPVEYOR_SCHEDULED_BUILD%"=="True" (
 cmake -DCMAKE_VERBOSE_MAKEFILE=ON -DPXSCENE_VERSION="edge" ..
 )
-
+	
 if "%APPVEYOR_SCHEDULED_BUILD%"=="" (
     if "APPVEYOR_REPO_TAG"=="false" (
         cmake -DCMAKE_VERBOSE_MAKEFILE=ON ..
     )
-	if "APPVEYOR_REPO_TAG"=="true" (
+	if "APPVEYOR_REPO_TAG"=="false" (
 	    @rem tag build, add build version :  Use ProductVersion and FILEVERSION from pxscene2d/src/win/pxscene.rc 
 		setlocal enabledelayedexpansion
 		for /f "tokens=1,* delims=]" %%a in ('find /n /v "" ^< "..\examples\pxScene2d\src\win\pxscene.rc" ^| findstr "FILEVERSION" ') do set "verInfo=%%b"
-		for  /f "tokens=2,* delims=\ " %%a in ("%verInfo%") do set "prodVer=%%a"
+		for /f "tokens=2,* delims=\ " %%a in ("%verInfo%") do set "prodVer=%%a"
 		set dotReplace=.
 		set prodVer=%prodVer:,=!dotReplace!%
 		echo %prodVer%
