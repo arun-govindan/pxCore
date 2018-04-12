6cb7ede3f9b3bb31577555b5240156bc1e01ae62
Govindan <arun.govindan@lnttechservices.com>
2018-04-12 14:44:39 -0400

    test

--- examples/pxScene2d/external/buildWindows.bat
+++ examples/pxScene2d/external/buildWindows.bat
@@ -6,26 +6,33 @@
  pause
  goto :eof
 )
+echo. =================================== starting of buildWindows
+time /t
 
 copy /y libjpeg-turbo-1.5.1\win_temp\* libjpeg-turbo-1.5.1\
 copy /y curl-7.40.0\include\curl\curlbuild-win.h curl-7.40.0\include\curl\curlbuild.h
 copy /y libpng-1.6.28\scripts\pnglibconf.h.prebuilt libpng-1.6.28\pnglibconf.h
 copy /y jpeg-9a\jconfig.vc jpeg-9a\jconfig.h
 
+echo. =================================== starting of buildWindows
+time /t
 cd vc.build\
 msbuild external.sln /p:Configuration=Release /p:Platform=Win32 /m
 cd ..
-
+echo. =================================== end of external solutiton
+time /t
 cd breakpad-chrome_55
 CALL gyp\gyp.bat src\client\windows\breakpad_client.gyp --no-circular-check
 cd src\client\windows
 msbuild breakpad_client.sln /p:Configuration=Release /p:Platform=Win32 /m
 cd ..\..\..\..\
-
+echo. =================================== end of breakpad
+time /t
 cd libnode-v6.9.0
 CALL vcbuild.bat x86 nosign
 cd ..
-
+echo. =================================== end of libnode
+time /t
 cd dukluv
 patch -p1 < patches/dukluv.git.patch
 mkdir build
@@ -33,3 +40,5 @@ cd build
 cmake ..
 cmake --build . --config Release -- /m
 cd ..
+echo. =================================== end of dukluv
+time /t
