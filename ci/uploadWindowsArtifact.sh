0639857e1de4794fb98137e10b0babcc4bd4fe99
arun-govindan <arun_govindan@comcast.com>
2018-06-01 13:05:07 -0400

    test

--- ci/uploadWindowsArtifact.sh
+++ ci/uploadWindowsArtifact.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo ">>>>>>>>>>>>>uploading artifact to build server"
+echo ">>>>>>>>>>>>>   uploading artifact to build server"
 API_URL="https://ci.appveyor.com/api"
 export projStr="$(curl -sS --header "Content-type: application/json" "https://ci.appveyor.com/api/projects/pxscene/pxcore/history?recordsNumber=20")"
 counter=0
@@ -7,8 +7,8 @@ echo ">>>>>>>>>>>>>>>>>before while loop"
 while [  $counter -lt 20 ]; do
   buildList=".builds[$counter].version"
   jobId=$(echo $projStr | jq -r  $buildList)
-  counter=$((counter+60))
-  echo ">>>>>>>>>>>>>>>>> trace 1"; 
+  counter=$((counter+1))
+  echo ">>>>>>>>>>>>>>>>> trace 1 : $jobId"; 
   if [ "$jobId" != null ] ; then
   build="$(curl -sS --header "Content-type: application/json" "https://ci.appveyor.com/api/projects/pxscene/pxcore/build/"$jobId)" 
   artifactStr=".build.jobs[0].artifactsCount"
@@ -18,9 +18,12 @@ while [  $counter -lt 20 ]; do
     if [ $artifactCounts -gt 0 ] ; then
     downloadArtifact="wget -q https://ci.appveyor.com/api/buildjobs/"$buildVer"/artifacts/pxscene-setup.exe"
     echo "Artifact count : $artifactCounts, Build version :  $buildVer, JobId : $(echo $build | jq -r .build.jobs[0].buildNumber)"
+    echo "$downloadArtifact"
     $downloadArtifact
+    if [ "$?" -ne 0 ] ; then
+      echo ">>>>>>>>>>>>>>> failed to upload the artifact."
+    fi
     #DOWNLOAD_ARTIFACT="$(curl -sS --header "Content-type: application/json" "https://ci.appveyor.com/api/buildjobs/"$buildVer"/artifacts/pxscene-setup.exe")" 
-    echo "::::wget -q https://ci.appveyor.com/api/buildjobs/"$buildVer"/artifacts/pxscene-setup.exe :::: " $downloadArtifact
     break;
     fi
   fi
