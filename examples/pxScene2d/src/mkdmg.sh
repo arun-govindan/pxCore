3388a3a612d5facebc90f5487267949c89964d46
arun-govindan <arun_govindan@comcast.com>
2018-07-23 09:38:33 -0400

    edge updates

--- examples/pxScene2d/src/mkdmg.sh
+++ examples/pxScene2d/src/mkdmg.sh
@@ -147,8 +147,13 @@ createDMG() {
   #applescript clauses to set icon positions within the dmg
   BACKGROUND_CLAUSE="set background picture of opts to file \".background:${BACKGROUND_FILE_NAME}\""
   REPOSITION_HIDDEN_FILES_CLAUSE="set position of every item to {theBottomRightX + 100, 100}"
+if [ "$TRAVIS_EVENT_TYPE" == "cron" ]
+  then  
+    POSITION_CLAUSE="${POSITION_CLAUSE}set position of item \"pxsceneEdge.app\" to {240, 140}"
+  else
     POSITION_CLAUSE="${POSITION_CLAUSE}set position of item \"pxscene.app\" to {240, 140}"
-  APPLICATION_CLAUSE="set position of item \"Applications\" to {240, 390}"
+  fi    
+APPLICATION_CLAUSE="set position of item \"Applications\" to {240, 390}"
 
   DMG_FILE="deploy/mac/pxscene.dmg"
   DMG_DIRNAME="$(dirname "${DMG_FILE}")"
