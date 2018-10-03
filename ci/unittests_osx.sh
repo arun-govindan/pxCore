f09a5f3e4b07fb7cdd4dfed20015d1f8027a2d3e
arun-govindan <arun_govindan@comcast.com>
2018-10-03 11:29:33 -0400

    Force travis validation to fail, when thread sanitizer raises warning

--- ci/unittests_osx.sh
+++ ci/unittests_osx.sh
@@ -30,61 +30,80 @@ grep "Global test environment tear-down" $TESTLOGS
 retVal=$?
 count=0
 while [ "$retVal" -ne 0 ] &&  [ "$count" -ne 300 ]; do
-	sleep 60;
-	grep "Global test environment tear-down" $TESTLOGS
-	retVal=$?
-	count=$((count+60))
-	echo "unittests running for $count seconds"
+  sleep 60;
+  grep "Global test environment tear-down" $TESTLOGS
+  retVal=$?
+  count=$((count+60))
+  echo "unittests running for $count seconds"
 
-	#check any crash happened, if so stop the loop
-	if [ "$retVal" -ne 0 ]
-	then
-		if [ -f "/tmp/pxscenecrash" ]
-		then
-			dumped_core=1
-			sudo rm -rf /tmp/pxscenecrash
-			break
-		fi
-	fi
+  #check any crash happened, if so stop the loop
+  if [ "$retVal" -ne 0 ]
+  then
+    if [ -f "/tmp/pxscenecrash" ]
+    then
+      dumped_core=1
+      sudo rm -rf /tmp/pxscenecrash
+      break
+    fi
+  fi
 #crash check ends
 done
 
 #check for corefile presence
 if [ "$dumped_core" -eq 1 ]
 then
-	echo "ERROR: Core Dump during unittests execution - $dumped_core"
-
-	$TRAVIS_BUILD_DIR/ci/check_dump_cores_osx.sh `pwd` `ps -ef | grep pxscene2dtests |grep -v grep|grep -v pxscene2dtests.sh|awk '{print $2}'` $TESTLOGS
-	retVal=$?
-	if [ "$retVal" -eq 1 ]
-	then
-		checkError $retVal "unittests execution failed" "Core dump" "Run unittests locally"
-	fi
+  echo "ERROR: Core Dump during unittests execution - $dumped_core"
+  $TRAVIS_BUILD_DIR/ci/check_dump_cores_osx.sh `pwd` `ps -ef | grep pxscene2dtests |grep -v grep|grep -v pxscene2dtests.sh|awk '{print $2}'` $TESTLOGS
+  retVal=$?
+  if [ "$retVal" -eq 1 ]
+  then
+    checkError $retVal "unittests execution failed" "Core dump" "Run unittests locally"
+  fi
 fi
 
 pxpid=`pgrep -n pxscene2dtests.sh`
 if [ -n "$pxpid" ]
 then
-	printf "\n\nCALLING ... kill -9 ${pxpid}  << pxscene2dtests.sh PID"
-	kill -9 $pxpid
+  printf "\n\nCALLING ... kill -9 ${pxpid}  << pxscene2dtests.sh PID"
+  kill -9 $pxpid
 fi
 
 errCause=""
+
+#check for thread sanitizer report
+
+grep -n "WARNING: ThreadSanitizer:" $TESTLOGS
+retVal=$?
+if [ "$retVal" -eq 0 ]
+then
+  if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+  then
+    errCause="Thread Sanitizer warning detected. Check the above logs"
+    echo "********************** PRINTING TEST LOG **************************"
+    cat $TESTLOGS
+    echo "************************** LOG ENDS *******************************"
+  else
+    errCause="Thread Sanitizer warning detected. Check the log file $TESTLOGS"
+  fi
+    checkError -1 "Thread Sanitizer warning detected" "$errCause" "Compile spark unit test with -DENABLE_THREAD_SANITIZER=ON option and test locally."
+fi
+
+
 #check for process hung
 grep "Global test environment tear-down" $TESTLOGS
 retVal=$?
 if [ "$retVal" -eq 1 ]
 then
-	if [ "$TRAVIS_PULL_REQUEST" != "false" ]
-	then
-		errCause="Either one or more tests failed. Check the above logs"
-		echo "********************** PRINTING TEST LOG **************************"
-                cat $TESTLOGS
-                echo "************************** LOG ENDS *******************************"
-        else
-		errCause="Either one or more tests failed. Check the log file $TESTLOGS"
-	fi 
-	checkError $retVal "unittests execution failed" "$errCause" "Rrun unittests locally"
+  if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+  then
+    errCause="Either one or more tests failed. Check the above logs"
+    echo "********************** PRINTING TEST LOG **************************"
+    cat $TESTLOGS
+    echo "************************** LOG ENDS *******************************"
+  else
+    errCause="Either one or more tests failed. Check the log file $TESTLOGS"
+  fi 
+    checkError $retVal "unittests execution failed" "$errCause" "Run unittests locally"
 fi
 
 #check for failed test
@@ -93,16 +112,16 @@ retVal=$?
 cd $TRAVIS_BUILD_DIR;
 if [ "$retVal" -eq 0 ] # "FAILED TEST" was found. 
 then
-	if [ "$TRAVIS_PULL_REQUEST" != "false" ]
-	then
-		errCause="Either one or more tests failed. Check the above logs"
-	        echo "********************** PRINTING TEST LOG **************************"
-                cat $TESTLOGS
-                echo "************************** LOG ENDS *******************************"
-        else
-		errCause="Either one or more tests failed. Check the log file $TESTLOGS"
-	fi 
-	checkError -1 "unittests execution failed" "$errCause" "Run unittests locally"
+  if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+  then
+    errCause="Either one or more tests failed. Check the above logs"
+    echo "********************** PRINTING TEST LOG **************************"
+    cat $TESTLOGS
+    echo "************************** LOG ENDS *******************************"
+  else
+    errCause="Either one or more tests failed. Check the log file $TESTLOGS"
+    fi 
+    checkError -1 "unittests execution failed" "$errCause" "Run unittests locally"
 else
-	exit 0;
+  exit 0;
 fi
