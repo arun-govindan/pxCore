647688401b8e07462038188b326ab2f30fc25b6a
arun-govindan <arun_govindan@comcast.com>
2018-10-10 09:09:35 -0400

    test

--- examples/pxScene2d/external/buildV8.sh
+++ examples/pxScene2d/external/buildV8.sh
@@ -2,14 +2,17 @@
 
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git depot_tools
 export PATH=$PWD/depot_tools/:$PATH
-
+echo "-------------- git clone completed"
 gclient
 
 fetch v8
+echo "------------- fetch completed"
 cd v8
 
 gclient sync --no-history --with_tags -r 6.9.351
 python tools/dev/v8gen.py x64.release -- is_debug=false v8_enable_i18n_support=false target_cpu=\"x64\" is_component_build=false v8_static_library=true use_custom_libcxx = false use_custom_libcxx_for_host = false
+
+echo "----------------- python completed"
 
 ninja -C out.gn/x64.release v8
 
