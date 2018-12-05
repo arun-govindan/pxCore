4169c5506549f0862743be649b3899c3874bb4f1
Govindan <arun.govindan@lnttechservices.com>
2018-12-05 06:13:58 -0500

    test

--- ci/execute_win.bat
+++ ci/execute_win.bat
@@ -5,7 +5,9 @@ pwd
 cd build-win32\_CPack_Packages\win32\NSIS\spark-setup
 taskkill /f /t /im Spark.exe
 
-start /B spark.exe https://px-apps.sys.comcast.net/pxscene-samples/examples/px-reference/gallery/answers.js
+start /B spark.exe https://px-apps.sys.comcast.net/pxscene-samples/examples/px-reference/gallery/answers.js > logs.txt
 
-timeout /t 10
+timeout  20
 taskkill /f /t /im Spark.exe
+
+cat logs.txt
