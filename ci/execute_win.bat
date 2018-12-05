7d66b3ecf15c4ac6ad6bad51e0916bde73150f2e
Govindan <arun.govindan@lnttechservices.com>
2018-12-05 04:24:45 -0500

    test

---
+++ ci/execute_win.bat
@@ -0,0 +1,11 @@
+@echo on
+@rem script to run spark with test runner
+pwd
+
+cd build-win32\_CPack_Packages\win32\NSIS\
+taskkill /f /t /im Spark.exe
+
+start /B spark.exe https://px-apps.sys.comcast.net/pxscene-samples/examples/px-reference/gallery/answers.js
+
+timeout /t 10
+taskkill /f /t /im Spark.exe
