4795b9ab4e1f52b4a6e46246d7d1df1038222ac8
Govindan <arun.govindan@lnttechservices.com>
2018-12-06 09:01:55 -0500

    add test runner

--- ci/execute_win.bat
+++ ci/execute_win.bat
@@ -1,13 +1,30 @@
 @echo on
 @rem script to run spark with test runner
-pwd
+echo %CD%
+
+rm -rf logs
+mkdir logs
+set CURRDIR=%CD%
+set LOGSDIR=%CD%\logs
+set TESTRUNNER="https://px-apps.sys.comcast.net/pxscene-samples/examples/px-reference/test-run/testRunner_v7.js"
 
 cd build-win32\_CPack_Packages\win32\NSIS\spark-setup
+@rem taskkill /f /t /im Spark.exe
+
+start /B spark.exe %TESTRUNNER%?tests=%CURRDIR%\tests\pxScene2d\testRunner\tests.json > %LOGSDIR%\exec_logs.txt
+
+set procCompleted=0
+:while
+   echo "----------------- %LOGSDIR%\exec_logs.txt"
+   grep -nr "TEST RESULTS: "  %LOGSDIR%\exec_logs.txt
+   set errVal=%errorlevel%
+   echo "the errVal : %errVal%"
+   if %errVal% EQU 0 goto :break
+   echo the proc value %procCompleted%
+   timeout /t 10
+   goto :while
+
+:break
+
+
 taskkill /f /t /im Spark.exe
-
-start /B spark.exe https://px-apps.sys.comcast.net/pxscene-samples/examples/px-reference/gallery/answers.js > logs.txt
-
-timeout  20
-taskkill /f /t /im Spark.exe
-
-cat logs.txt
