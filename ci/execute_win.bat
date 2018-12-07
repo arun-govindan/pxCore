7253b2cf85ee6150b8cf5010e38fb3b4cc413bf3
Govindan <arun.govindan@lnttechservices.com>
2018-12-07 09:52:28 -0500

    test

--- ci/execute_win.bat
+++ ci/execute_win.bat
@@ -15,9 +15,10 @@ start /B spark.exe %TESTRUNNER%?tests=%CURRDIR%\tests\pxScene2d\testRunner\tests
 
 set procCompleted=0
 :while
+   
+   tasklist |grep Spark
    grep -nr "TEST RESULTS: "  %LOGSDIR%\exec_logs.txt
    set errVal=%errorlevel%
-   tasklist |grep Spark
    if %errVal% EQU 0 goto :break
    sleep 30
    goto :while
